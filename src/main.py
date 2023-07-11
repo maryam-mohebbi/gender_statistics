4f4e6b9c5b1e36d9ad9a2698949cb2ddede7213d
Maryam Mohebbi <maryam.mohebbi.2@gmail.com>
2023-07-11 12:45:07 +0200

    merge two block codes

--- src/main.py
+++ src/main.py
@@ -1,142 +1,400 @@
-import plotly.graph_objects as go
-import dash_html_components as html
-import dash_core_components as dcc
-from dash.dependencies import Input, Output
-from dash import html
-from dash import dcc
-import dash
-import plotly.express as px
 import pandas as pd
+import plotly.express as px
+from dash import Dash
+from dash import dcc, html
+from dash.dependencies import Input, Output, State
+import plotly.graph_objs as go
+from plotly.subplots import make_subplots
+from math import ceil
+import numpy as np
 
-df = pd.read_csv('../data/data.csv')
-df_melted = pd.melt(df,
-                    id_vars=['Series Name', 'Series Code',
-                             'Country Name', 'Country Code'],
-                    var_name='Year',
-                    value_name='Value')
 
-df_melted['Year'] = df_melted['Year'].str.slice(0, 4).astype(int)
+def binary_categories_bar_creation(filtered_df, category_code, year_range, number_of_country, country):
+    binary_series_df = filtered_df[(filtered_df['Series Code'] == category_code) & (
+        filtered_df['Country Name'] == country)]
 
-app = dash.Dash(__name__)
+    x_years_all = binary_series_df.loc[:,
+                                       f'{year_range[0]} [YR{year_range[0]}]':f'{year_range[1]} [YR{year_range[1]}]'].columns
+    x_years_all = [int(x[0:4]) for x in x_years_all]
+    x_values = []
+    for year in x_years_all:
+        if year % 5 == 0 and str(year)[-1] == '5':
+            x_values.append(year)
+        else:
+            x_values.append(None)
 
-country_codes = df_melted['Country Code'].unique()
+    y_values_all = binary_series_df.loc[:, f'{year_range[0]} [YR{year_range[0]}]':
+                                           f'{year_range[1]} [YR{year_range[1]}]'].values[0]
+    y_values = []
+    for year, val in zip(x_years_all, y_values_all):
+        if year % 5 == 0 and str(year)[-1] == '5':
+            y_values.append(val)
+        else:
+            y_values.append(None)
+    y_values_final = []
+    for y in y_values:
+        if y == 1.0:
+            y_values_final.append(2)
+        elif y == 0.0:
+            y_values_final.append(1)
+        else:
+            y_values_final.append(0)
+
+    bar_offset_shift = None
+    if number_of_country == 0:
+        bar_offset_shift = -1
+    elif number_of_country == 1:
+        bar_offset_shift = -2
+    elif number_of_country == 2:
+        bar_offset_shift = 0
+    elif number_of_country == 3:
+        bar_offset_shift = 1
+
+    binary_trace = go.Bar(
+        x=x_years_all,
+        y=y_values_final,
+        name=country,
+        yaxis='y2',
+        marker_color=country_colors[number_of_country],
+        width=1,
+        offset=bar_offset_shift
+    )
+    return binary_trace
+
+
+def binary_categories_hist_creation(filtered_df, category_code, year_range, number_of_country, country):
+    series_df = filtered_df[(filtered_df['Series Code'] == category_code) & (
+        filtered_df['Country Name'] == country)]
+
+    x_values = series_df.loc[:,
+                             f'{year_range[0]} [YR{year_range[0]}]':f'{year_range[1]} [YR{year_range[1]}]'].columns
+    x_values = [x[0:4] for x in x_values]
+    y_values = series_df.loc[:, f'{year_range[0]} [YR{year_range[0]}]':
+                             f'{year_range[1]} [YR{year_range[1]}]'].values[0]
+    y_values_fixed = []
+    no_values_indexes = []
+    # existed_value =
+    for idx, y_value in enumerate(y_values):
+        if idx == 0 or np.isnan(y_value) == False:
+            existed_value = y_value
+            y_values_fixed.append(existed_value)
+        elif not y_value and y_value != 0:
+            y_values_fixed.append(existed_value)
+    name_of_graph = country
+    trace = go.Scatter(
+        x=x_values,
+        y=y_values_fixed,
+        mode='lines',
+        name=name_of_graph,
+        yaxis='y1',
+        marker_color=country_colors[number_of_country],
+        showlegend=True
+    )
+    return trace
+
+
+def create_return_for_category(data, x_values, category_code):
+    return {
+        'data': data,
+        'layout': go.Layout(
+            title=binary_series_data[category_code],
+            xaxis=dict(title='Year',
+                       tickangle=0,
+                       showgrid=True,
+                       range=[1970, 2021],
+                       tickvals=[i for i in range(int(x_values[0]), int(x_values[-1]), 5)]),
+            yaxis2=dict(title='Answer',
+                        overlaying='y',
+                        tickangle=-90,
+                        range=[0, 2],
+                        showgrid=True,
+                        tickvals=[1, 2],
+                        ticktext=['no', 'yes']
+                        ),
+            hovermode='closest'
+        )
+    }
+
+
+def create_return_for_hist_category(data, x_values, category_code):
+    if category_code == 'SG.LAW.INDX.EN':
+        ytitle = 'Indicator'
+        yrange = [-5, 140]
+        ytickvals = [0, 50, 100]
+        yticktext = ['0', '50', '100']
+    elif category_code == 'SE.TER.ENRR.FE':
+        ytitle = '% gross'
+        yrange = [-5, 140]
+        ytickvals = [0, 50, 100]
+        yticktext = ['0', '50', '100']
+
+    return {
+        'data': data,
+        'layout': go.Layout(
+            title=binary_series_data[category_code],
+            xaxis={'title': 'Year'},
+            yaxis=dict(title=ytitle,
+                       showgrid=False,
+                       range=yrange,
+                       tickvals=ytickvals,
+                       ticktext=yticktext
+                       ),
+            hovermode='closest',
+        )
+    }
+
+
+series_data = {
+    'NY.GDP.MKTP.CD': 'GDP (current US$)'
+}
+
+binary_series_data = {'SG.GET.JOBS.EQ': 'A woman can get a job in the same way as a man',
+                      'SG.IND.WORK.EQ': 'A woman can work in an industrial job in the same way as a man',
+                      'SG.LAW.NODC.HR': 'The law prohibits discrimination in employment based on gender',
+                      'SE.TER.ENRR.FE': 'School enrollment, tertiary, female (% gross)',
+                      'SG.LAW.INDX.EN': 'Women, Business and the Law: Entrepreneurship Indicator Score (scale 1-100)',
+                      'SG.CNT.SIGN.EQ': 'A woman can sign a contract in the same way as a man',
+                      }
+
+country_colors = {0: '#fed98e',
+                  1: '#fe9929',
+                  2: '#d95f0e',
+                  3: '#993404'}
+
+df = pd.read_csv('../data/cleaned_data.csv')
+
+df_series = df[['Series Name', 'Country Name'] +
+               [col for col in df if col.startswith('19') or col.startswith('20')]]
+
+df_series = df_series.melt(id_vars=['Series Name', 'Country Name'],
+                           var_name='Year', value_name='Value')
+
+df_series['Year'] = df_series['Year'].str.extract('(\d+)').astype(int)
+
+df_series = df_series.pivot_table(index=['Country Name', 'Year'],
+                                  columns='Series Name', values='Value').reset_index()
+
+df_series.columns.name = ''
+df_series.rename(columns={'Country Name': 'Country'}, inplace=True)
+
+
+group_features = ['Population, total',
+                  'Population, female',
+                  'Population, male',]
+
+regions = {
+    "Europe": ["United Kingdom", "France", "Germany", "Italy"],
+    "Middle East": ["Saudi Arabia", "Iran, Islamic Rep.", "Israel", "Turkiye"],
+    "Asia": ["China", "Japan", "India", "Vietnam"],
+    "Africa": ["Egypt, Arab Rep.", "South Africa", "Nigeria", "Kenya"],
+    "South America": ["Brazil", 'Argentina', 'Venezuela, RB', "Uruguay"],
+    "North and middle America": ['United States', 'Canada', 'Mexico', 'Panama'],
+
+}
+all_countries = df_series['Country'].unique().tolist()
+
+
+df_series_original = df_series.copy()
+
+
+app = Dash(__name__)
+
 
 app.layout = html.Div([
     dcc.Dropdown(
         id='country-dropdown',
-        options=[{'label': i, 'value': i} for i in country_codes],
-        value='USA'
+        options=[{'label': country, 'value': country}
+                 for country in all_countries],
+        multi=True,
+        value=[]
     ),
+    dcc.RadioItems(
+        id='region-radio',
+        options=[{'label': region, 'value': region}
+                 for region in regions.keys()],
+        value=None
+    ),
+    dcc.Graph(id='animated-chart'),
+
     dcc.RangeSlider(
         id='year-slider',
-        min=1960,
-        max=2022,
+        min=1970,
+        max=2021,
         step=1,
-        value=[1960, 2022],
+        value=[1970, 2021],
         marks={i: str(i) for i in range(1960, 2023, 2)}
     ),
     dcc.Graph(id='indicator-graph'),
-    dcc.Graph(id='binary-indicator-graph')
+    dcc.Graph(id='sg_get_jobs_eq_binary-indicator-graph'),
+    dcc.Graph(id='sg_get_work_eq_binary-indicator-graph'),
+    dcc.Graph(id='sg_law_nodc_hr_binary-indicator-graph'),
+    dcc.Graph(id='se_ter_enrr_fe_binary-indicator-graph'),
+    dcc.Graph(id='sg_law_indx_en_binary-indicator-graph'),
+    dcc.Graph(id='sg_cnt_sign_eq_binary-indicator-graph')
 ])
 
-# Define numerical and binary series
-series_codes = ['SP.ADO.TFRT',
-                'SP.POP.DPND',
-                'NY.GDP.MKTP.CD',
-                'NY.GDP.MKTP.KD.ZG',
-                'NY.GDP.PCAP.CD',
-                'SP.DYN.LE00.IN',
-                'SP.DYN.LE00.FE.IN',
-                'SP.DYN.LE00.MA.IN'
-                ]
+
+@app.callback(
+    Output('country-dropdown', 'value'),
+    [Input('region-radio', 'value')],
+    [State('country-dropdown', 'options')]
+)
+def update_dropdown_values(selected_region, available_options):
+    if selected_region is None:
+        return []
+    else:
+        region_countries = regions[selected_region]
+        return [country['value'] for country in available_options if country['value'] in region_countries]
 
 
-series_names = ['Adolescent fertility rate (births per 1,000 women ages 15-19)',
-                'Age dependency ratio (% of working-age population)',
-                'GDP (current US$)',
-                'GDP growth (annual %)',
-                'GDP per capita (Current US$)',
-                'Life expectancy at birth, total (years)',
-                'Life expectancy at birth, female (years)',
-                'Life expectancy at birth, male (years)'
-                ]
+@app.callback(
+    Output('animated-chart', 'figure'),
+    [Input('country-dropdown', 'value')]
+)
+def population_chart(selected_countries):
+    if len(selected_countries) > 10:
+        return go.Figure()
+    else:
+        filtered_df_series = df_series_original[df_series_original['Country'].isin(
+            selected_countries)]
 
+        melted_df_series = pd.melt(filtered_df_series, id_vars=['Country', 'Year'],
+                                   value_vars=group_features,
+                                   var_name='Feature', value_name='Value')
+        fig1 = px.bar(melted_df_series,
+                      x="Country",
+                      y='Value',
+                      color="Feature",
+                      animation_frame="Year",
+                      animation_group="Country",
+                      labels={'Value': 'Population'},
+                      title='Population Change Over Time',
+                      barmode='group')
+        fig1.update_layout(yaxis_range=[0, melted_df_series['Value'].max()])
 
-binary_series_codes = ['SG.APL.PSPT.EQ',
-                       'SG.HLD.HEAD.EQ',
-                       'SG.LOC.LIVE.EQ',
-                       'SG.OBT.DVRC.EQ']
-binary_series_names = ['Can a woman apply for a passport in the same way as a man?',
-                       'Can a woman be head of household in the same way as a man?',
-                       'Can a woman choose where to live in the same way as a man?',
-                       'Can a woman obtain a judgment of divorce in the same way as a man?']
+        max_pop = melted_df_series[melted_df_series['Feature'] == 'Population, total'].groupby('Country')[
+            'Value'].max()
+        for country in selected_countries:
+            fig1.add_trace(
+                go.Scatter(x=[country], y=[max_pop[country]],
+                           mode='markers',
+                           marker=dict(size=10, color='Red'),
+                           showlegend=False)
+            )
+
+        return fig1
 
 
 @app.callback(
     [Output('indicator-graph', 'figure'),
-     Output('binary-indicator-graph', 'figure')],
+     Output('sg_get_jobs_eq_binary-indicator-graph', 'figure'),
+     Output('sg_get_work_eq_binary-indicator-graph', 'figure'),
+     Output('sg_law_nodc_hr_binary-indicator-graph', 'figure'),
+     Output('se_ter_enrr_fe_binary-indicator-graph', 'figure'),
+     Output('sg_law_indx_en_binary-indicator-graph', 'figure'),
+     Output('sg_cnt_sign_eq_binary-indicator-graph', 'figure')],
     [Input('country-dropdown', 'value'),
      Input('year-slider', 'value')]
 )
-def update_graph(country_code, year_range):
-    filtered_df = df[df['Country Code'] == country_code]
+def update_graph(selected_countries, year_range):
+    if not selected_countries:
+        return go.Figure(), go.Figure(), go.Figure(), go.Figure(), go.Figure(), go.Figure(), go.Figure()
+
+    filtered_df = df[df['Country Name'].isin(selected_countries)]
     traces = []
-    binary_traces = []
+    sg_get_jobs_eq_traces = []
+    sg_get_work_eq_traces = []
+    sg_law_nodc_hr_traces = []
+    sg_sec_enrr_fe_traces = []
+    sg_law_indx_en_traces = []
+    sg_cnt_sign_eq_traces = []
+    y_scatter_max = []
 
-    for series_code, series_name in zip(series_codes, series_names):
-        series_df = filtered_df[filtered_df['Series Code'] == series_code]
-        x_values = series_df.loc[:,
-                                 f'{year_range[0]} [YR{year_range[0]}]':f'{year_range[1]} [YR{year_range[1]}]'].columns
+    for number_of_country, country in enumerate(selected_countries):
+        first_series_df = filtered_df[filtered_df['Series Code']
+                                      == 'NY.GDP.MKTP.CD']
+
+        x_values = first_series_df.loc[:,
+                                       f'{year_range[0]} [YR{year_range[0]}]':f'{year_range[1]} [YR{year_range[1]}]'].columns
         x_values = [x[0:4] for x in x_values]
-        y_values = series_df.loc[:, f'{year_range[0]} [YR{year_range[0]}]':
-                                 f'{year_range[1]} [YR{year_range[1]}]'].values[0]
-        traces.append(go.Scatter(
+        y_values = first_series_df[first_series_df['Country Name'] == country].loc[:,
+                                                                                   f'{year_range[0]} [YR{year_range[0]}]':f'{year_range[1]} [YR{year_range[1]}]'].values[0]
+
+        y_values_fixed = []
+        for idx, y_value in enumerate(y_values):
+            if idx == 0 or np.isnan(y_value) == False:
+                existed_value = y_value
+                y_values_fixed.append(existed_value)
+            elif not y_value and y_value != 0:
+                y_values_fixed.append(existed_value)
+
+        y_scatter_max.append(max(y_values))
+        name_of_graph = country
+        trace = go.Scatter(
             x=x_values,
-            y=y_values,
+            y=y_values_fixed,
             mode='lines',
-            name=series_name
-        ))
-
-    for series_code, series_name in zip(binary_series_codes, binary_series_names):
-        binary_series_df = filtered_df[filtered_df['Series Code']
-                                       == series_code]
-        x_values = binary_series_df.loc[:,
-                                        f'{year_range[0]} [YR{year_range[0]}]':f'{year_range[1]} [YR{year_range[1]}]'].columns
-        x_values = [x[0:4] for x in x_values]
-        y_values = binary_series_df.loc[:,
-                                        f'{year_range[0]} [YR{year_range[0]}]':f'{year_range[1]} [YR{year_range[1]}]'].values[0]
-
-        # Mapping values to 1 (Yes), 0 (No), and -1 (No data)
-        y_values = [1 if y == '1' else (
-            0 if y == '0' else -1) for y in y_values]
-
-        binary_traces.append(go.Bar(
-            x=x_values,
-            y=y_values,
-            name=series_name,
-        ))
-
-    return {
-        'data': traces,
-        'layout': go.Layout(
-            xaxis={'title': 'Year'},
-            yaxis={'title': 'Indicator Value'},
-            title='Country Indicators Over Time',
-            hovermode='closest'
+            name=name_of_graph,
+            yaxis='y1',
+            marker_color=country_colors[number_of_country],
+            showlegend=True
         )
-    }, {
-        'data': binary_traces,
-        'layout': go.Layout(
-            xaxis={'title': 'Year'},
-            yaxis={'title': 'Indicator Value',
-                   # Adjust the range to include all categories
-                   'range': [-1.5, 1.5],
-                   'tickvals': [-1, 0, 1],
-                   'ticktext': ['No data', 'No', 'Yes']},
-            title='Binary Indicators Over Time',
-            hovermode='closest'
-        )
-    }
+        traces.append(trace)
+
+        sg_get_jobs_eq_traces.append(binary_categories_bar_creation(
+            filtered_df, 'SG.GET.JOBS.EQ', year_range, number_of_country, country))
+        sg_get_work_eq_traces.append(binary_categories_bar_creation(
+            filtered_df, 'SG.IND.WORK.EQ', year_range, number_of_country, country))
+        sg_law_nodc_hr_traces.append(binary_categories_bar_creation(
+            filtered_df, 'SG.LAW.NODC.HR', year_range, number_of_country, country))
+        sg_cnt_sign_eq_traces.append(binary_categories_bar_creation(
+            filtered_df, 'SG.CNT.SIGN.EQ', year_range, number_of_country, country))
+        sg_sec_enrr_fe_traces.append(binary_categories_hist_creation(
+            filtered_df, 'SE.TER.ENRR.FE', year_range, number_of_country, country))
+        sg_law_indx_en_traces.append(binary_categories_hist_creation(
+            filtered_df, 'SG.LAW.INDX.EN', year_range, number_of_country, country))
+
+    if selected_countries != 'Cameroon, Egypt, Kenya, Nigeria':
+        shift = int(round(int(max(y_scatter_max)) * 0.25, -12))
+        positive_tickvals = [i for i in range(
+            0, int(max(y_scatter_max)), shift)]
+        max_range = max(y_scatter_max) + 1000000000000
+        min_range = -max(y_scatter_max) * 0.4
+    else:
+        shift = int(round(int(max(y_scatter_max)) * 0.33, -10))
+        positive_tickvals = [i for i in range(
+            0, int(max(y_scatter_max)), shift)]
+        max_range = max(y_scatter_max) + 10000000000
+        min_range = -max(y_scatter_max) * 0.4
+
+    return (
+        {
+            'data': traces,
+            'layout': go.Layout(
+                title='<b>Prosperity of the economy depends on the participation of women<b>',
+                xaxis={'title': 'Year'},
+                yaxis=dict(
+                    title='GDP (current US$)',
+                    showgrid=False,
+                    range=[min_range, max_range],
+                    tickvals=positive_tickvals,
+                ),
+                hovermode='closest',
+            )
+        },
+        create_return_for_category(
+            sg_get_jobs_eq_traces, x_values, 'SG.GET.JOBS.EQ'),
+        create_return_for_category(
+            sg_get_work_eq_traces, x_values, 'SG.IND.WORK.EQ'),
+        create_return_for_category(
+            sg_law_nodc_hr_traces, x_values, 'SG.LAW.NODC.HR'),
+        create_return_for_hist_category(
+            sg_sec_enrr_fe_traces, x_values, 'SE.TER.ENRR.FE'),
+        create_return_for_hist_category(
+            sg_law_indx_en_traces, x_values, 'SG.LAW.INDX.EN'),
+        create_return_for_category(
+            sg_cnt_sign_eq_traces, x_values, 'SG.CNT.SIGN.EQ')
+    )
 
 
 if __name__ == '__main__':
