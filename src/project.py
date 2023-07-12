9f9af89059fc17251c9b70be80e64ffc5a8f8015
Raj Waghela <132276418+Rajwaghela369@users.noreply.github.com>
2023-07-12 11:52:31 +0200

    Add files via upload

--- src/project.py
+++ src/project.py
@@ -120,7 +120,9 @@ app = dash.Dash(__name__)
 # Define the layout of the app
 app.layout = html.Div(
     children=[
-        html.H1('Birth Vs Death Rate per 1000 people'), # birth vs death
+        html.H1('Scatter plot: Birth Vs Death Rate per 1000 people'), # birth vs death
+        html.H4('Birth Rate: indicates the number of birth occurring during the year, per 1,000 population estimated at midyear'),
+        html.H4('Death Rate: indicates the number of deaths occurring during the year, per 1,000 population estimated at midyear'),
         dcc.Graph(
             id='animated-bubble-plot',
             figure=px.scatter(df, x='Birth rate, crude (per 1,000 people)', y='Death rate, crude (per 1,000 people)',
@@ -137,8 +139,9 @@ app.layout = html.Div(
             )
         ),
 
-        html.Br(), 
+        html.Br(),
         html.H1('Fertility Rate Line Plot'), # fertility plots
+        html.H4('Fertility rate: number of live births per 1,000 women of reproductive age (ages 15 to 49 years) per year'),
         dcc.RadioItems(
             id='region-radio',
             options=[{'label': region, 'value': region} for region in formatted_data['Region'].unique()],
@@ -148,12 +151,20 @@ app.layout = html.Div(
         dcc.Graph(id='line-plot'),
 
         html.Br(),
-        html.H1('Mortality Rates'),
+        html.H1('Mortality Rates Stacked area charts'),
+        html.H4('Mortality Rate: units of deaths per 1,000 individuals per year'),
+        html.H4('Adult male Mortality Rate: units of deaths per 1,000 adult male per year'),
+        html.H4('Adult female Mortality Rate: units of deaths per 1,000 adult female per year'),
         html.Div(mor_plots), # mortality plots
         html.Br(),
-        html.H1('Infants Death'), # infants death plots
+        html.H1('Infants Death Stacked Area charts'), # infants death plots
+        html.H4('Infants Death: total no of infants death'),
+        html.H4('Infants Death in male: total no of infants death in male'),
+        html.H4('Infants Death in female: total no of infants death in female'),
         html.Div(in_plots),
-        html.H1('Immunization'), # immunization heat maps
+        html.H1('Immunization Heatmaps'), # immunization heat maps
+        html.H4('DPT vaccine: protects children and adults against diphtheria (D), pertussis (P, also known as whooping cough) and tetanus (T)'),
+        html.H4('measures in percentage'),
         dcc.Graph(
             id='immunization-heatmap',
             figure=px.imshow(
@@ -168,6 +179,8 @@ app.layout = html.Div(
                 width=1500
             )
         ),
+        html.H4('Measles: a highly contagious, serious airborne disease caused by a virus that can lead to severe complications and death.'),
+        html.H4('measures in percentage'),
         dcc.Graph(
                 id='immunization-heatmap',
                 figure=px.imshow(
@@ -183,7 +196,8 @@ app.layout = html.Div(
                 )
         ),
         html.Br(),
-        html.H1('Survival to age 65, (% of cohort)'), # survival sactter plot
+        html.H1('Scatter plot: Survival to age 65, (% of cohort)'), # survival sactter plot
+        html.H4('Survival to age 65, in male and female, measures in percentage'),
         dcc.Graph(
             id='animated-bubble-plot',
             figure=px.scatter(df, x='Survival Rate (Male)', y='Survival Rate (Female)',
